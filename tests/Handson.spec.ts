b24c9dbf3eec330b8e42f31d9c778783a6aa8443
DharshiniJeyachandran <dharshini.jc@gmail.com>
2025-10-12 14:23:51 +0200

    adding handson test cases

--- tests/Handson.spec.ts
+++ tests/Handson.spec.ts
@@ -1,32 +1,71 @@
-import {test,expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
+import { sign } from "crypto";
 
-test('handson', async ({page}) => {
-    await page.goto("https://www.saucedemo.com/");
-    await expect(page).toHaveTitle("Swag Labs");
-    await page.getByPlaceholder("Username").fill("standard_user");
-    await page.getByPlaceholder("Password").fill("secret_sauce");
-    await page.getByRole('button', {name :'Login'}).click();
-    await page.waitForTimeout(5000);
+test("handson", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await expect(page).toHaveTitle("Swag Labs");
+  await page.getByPlaceholder("Username").fill("standard_user");
+  await page.getByPlaceholder("Password").fill("secret_sauce");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.waitForTimeout(5000);
+});
+
+test("Assertions", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await expect(page).toHaveTitle("Swag Labs");
+  await expect(page).toHaveURL("https://www.saucedemo.com/");
+  const userName = page.getByRole("textbox", { name: "Username" });
+  await expect(userName).toBeEnabled();
+  const password = page.getByRole("textbox", { name: "Password" });
+  await expect(password).toBeEnabled();
+  const login = page.getByRole("button", { name: "Login" });
+  await expect(login).toBeVisible();
+  await userName.fill("standard_user");
+  await password.fill("secret_sauce");
+
+  await expect(page.locator("#login_credentials h4")).toHaveText(
+    "Accepted usernames are:"
+  );
+  await expect(page.locator(".login_password h4")).toContainText(
+    "Password for all users:"
+  );
+  await login.click();
+  await page.screenshot({
+    path: "screenshots/after-login.png",
+    fullPage: true,
+  });
+});
+
+test.only("Amazon validations", async ({ page }) => {
+  await page.goto("https://www.amazon.com.be/");
+  await page.getByTitle("English").click();
+  await page.locator("#sp-cc-rejectall-link").click();
+  const SignIn = await page.getByRole("link", {name :"Hello, sign in"});
+  await expect (SignIn).toBeVisible();
+
+  const all = await page.getByRole("button", {name: "Open All Categories Menu"});
+  await all.click();
+  await page.waitForTimeout(2000);
+  await expect(page.getByRole("link", {name: "Best Sellers"})).toBeVisible();
+  await (page.getByRole("link", {name: "Best Sellers"})).click();
+  await page.screenshot({path: 'Screenshots/amazonBestSeller.png'});
+
+  const bestSeller = await page.getByRole("heading", {name : "Amazon Best Sellers"});
+  await expect (bestSeller).toBeVisible();
+
+  const beauty = await page.getByRole("link", {name: "Beauty"});
+  await beauty.click();
+  await page.screenshot({path: "Screenshots/amazon/beauty.png"});
+
+  await expect(page.getByRole("heading", {name :"Best Sellers in Beauty"})).toBeVisible();
+  const premiumBeauty = await page.getByRole("link", {name: "Premium Beauty"});
+  await premiumBeauty.click();
+  
+  await expect(page.getByRole("img", {name: "Premium Beauty"})).toBeVisible();
+  await expect(page.getByRole("img", {name: "Kerastase", exact:true})).toBeVisible();
+
+  await (page.getByText("All Discounts")).click();
+  await page.locator("#searchDropdownBox").selectOption({label: "Baby"});
+
+  await expect(page.getByRole("link", {name: "0 items in shopping basket"})).toBeVisible();
 })
-
-test.only('Assertions', async ({page}) => {
-      await page.goto("https://www.saucedemo.com/");
-      await expect(page).toHaveTitle("Swag Labs");
-      await expect(page).toHaveURL("https://www.saucedemo.com/");
-      const userName = page.getByRole('textbox', {name: 'Username'});
-      await expect (userName).toBeEnabled();
-      const password = page.getByRole('textbox', {name :'Password'});
-      await expect(password).toBeEnabled();
-      const login = page.getByRole('button', {name:'Login'});
-      await expect (login).toBeVisible();
-      await userName.fill("standard_user");
-      await password.fill("secret_sauce");
-
-
-    await expect(page.locator("#login_credentials h4")).toHaveText("Accepted usernames are:");
-    await expect(page.locator(".login_password h4")).toContainText("Password for all users:");
-       await login.click();
-      await page.screenshot({ path: 'screenshots/after-login.png', fullPage: true });
-
-})
-
